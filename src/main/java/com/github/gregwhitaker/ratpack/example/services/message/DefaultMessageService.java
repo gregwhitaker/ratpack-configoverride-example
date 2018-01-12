c813f37771d6133614b7a763669936dbfe9f365f
gregwhitaker <gwhitake@gmail.com>
2018-01-12 15:30:41 -0800

    Doing work

--- src/main/java/com/github/gregwhitaker/ratpack/example/services/message/DefaultMessageService.java
+++ src/main/java/com/github/gregwhitaker/ratpack/example/services/message/DefaultMessageService.java
@@ -1,17 +1,26 @@
 package com.github.gregwhitaker.ratpack.example.services.message;
 
+import com.github.gregwhitaker.ratpack.example.core.config.MessagesConfiguration;
+import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
 @Singleton
 public class DefaultMessageService implements MessageService {
 
+    private MessagesConfiguration config;
+
+    @Inject
+    public DefaultMessageService(MessagesConfiguration config) {
+        this.config = config;
+    }
+
     @Override
     public String message1() {
-        return null;
+        return config.message1;
     }
 
     @Override
     public String message2() {
-        return null;
+        return config.message2;
     }
 }
