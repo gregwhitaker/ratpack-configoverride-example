c459c3a06c96fa6c6dd012ab33e7e82ac89cd366
gregwhitaker <gwhitake@gmail.com>
2018-01-12 16:05:05 -0800

    Doing work

--- src/main/java/com/github/gregwhitaker/ratpack/example/DatabaseConfiguration.java
+++ src/main/java/com/github/gregwhitaker/ratpack/example/DatabaseConfiguration.java
@@ -1,17 +1,32 @@
-package com.github.gregwhitaker.ratpack.example.core.config;
+package com.github.gregwhitaker.ratpack.example;
 
 /**
  * Configuration object that holds messages to return to the user.
  */
 public class DatabaseConfiguration {
 
+    /**
+     * Database host
+     */
     public String host;
 
+    /**
+     * Database port
+     */
     public int port;
 
+    /**
+     * Database username
+     */
     public String username;
 
+    /**
+     * Database password
+     */
     public String password;
 
+    /**
+     * Database name
+     */
     public String database;
 }
--- src/main/java/com/github/gregwhitaker/ratpack/example/Main.java
+++ src/main/java/com/github/gregwhitaker/ratpack/example/Main.java
@@ -1,6 +1,5 @@
 package com.github.gregwhitaker.ratpack.example;
 
-import com.github.gregwhitaker.ratpack.example.core.config.DatabaseConfiguration;
 import ratpack.jackson.Jackson;
 import ratpack.server.BaseDir;
 import ratpack.server.RatpackServer;
@@ -13,8 +12,8 @@ public class Main {
     public static void main(String... args) throws Exception {
         RatpackServer.start(s -> s
                 .serverConfig(c -> c
-                        .env()
                         .yaml("config.yaml")
+                        .env()
                         .require("/database", DatabaseConfiguration.class)
                         .baseDir(BaseDir.find()).build())
                 .handlers(chain -> chain
